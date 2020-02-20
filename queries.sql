8c729f449a25daa16ec85847d58a2adf3641609e
igor <embiggenerd@gmail.com>
2020-02-20 17:08:12 -0500

    cleanup

--- queries.sql
+++ queries.sql
@@ -4,14 +4,13 @@
 SELECT ProductName, CategoryName 
 FROM product
 JOIN category on product.CategoryId = category.Id
-LIMIT 77
+
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
 SELECT o.Id, s.CompanyName, o.OrderDate
 FROM [order] as o
 JOIN shipper as s 
 ON s.Id = o.ShipVia
 WHERE o.OrderDate < '2012-08-09'
-LIMIT 429
 
 -- Display the name and quantity of the products ordered in order 
 -- with Id 10251. Sort by ProductName. Shows 3 records.
@@ -20,7 +19,6 @@ FROM OrderDetail As o
 JOIN Product As p
 ON o.ProductId = p.Id
 WHERE o.OrderId = 10251
-
 
 -- Display the OrderID, Customer's Company Name and 
 -- the employee's LastName for every order. All columns 
@@ -31,4 +29,3 @@ JOIN customer as c
 ON c.Id = o.CustomerId
 Join employee as e
 On e.Id = o.EmployeeId
-LIMIT 16789
