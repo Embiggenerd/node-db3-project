d137d3021d4468d8d43b29327c2a99813f8e236e
igor <embiggenerd@gmail.com>
2020-02-20 15:54:37 -0500

    find by id tested

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -8,7 +8,7 @@ const db = knex(config.development);
 const find = () => db('schemes').select('*')
 
 const findById = async id => {
-    const found = await  db('schemes').select('*').where({ id })
+    const found = await db('schemes').select('*').where({ id })
 
     if (found.length === 0) {
         return null
@@ -17,7 +17,25 @@ const findById = async id => {
     return found
 }
 
+const findSteps = async id => {
+    console.log('findSteps', id)
+    try {
+
+        const scheme = await db('schemes').select('scheme_name').where({ id })
+
+        const steps = await db('steps').select('id', 'step_number', 'instructions').where({ scheme_id: id })
+
+        const combined = steps.map(step => ({ ...step, ...scheme[0] }))
+        return combined
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+
+
 module.exports = {
     find,
-    findById
+    findById,
+    findSteps
 }
