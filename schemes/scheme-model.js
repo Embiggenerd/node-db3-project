8d40b6257c424150b75731b0458ea482e7775f0a
igor <embiggenerd@gmail.com>
2020-02-20 15:38:19 -0500

    find by id tested

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -7,9 +7,17 @@ const db = knex(config.development);
 
 const find = () => db('schemes').select('*')
 
+const findById = async id => {
+    const found = await  db('schemes').select('*').where({ id })
 
+    if (found.length === 0) {
+        return null
+    }
+
+    return found
+}
 
 module.exports = {
     find,
-
+    findById
 }
