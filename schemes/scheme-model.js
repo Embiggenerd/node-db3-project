92242b6c207aed42d909cf6cb3145e643d3633f6
igor <embiggenerd@gmail.com>
2020-02-20 15:26:05 -0500

    find helper tested

---
+++ schemes/scheme-model.js
@@ -0,0 +1,15 @@
+const knex = require('knex');
+
+const config = require('../knexfile.js');
+
+// we must select the development object from our knexfile
+const db = knex(config.development);
+
+const find = () => db('schemes').select('*')
+
+
+
+module.exports = {
+    find,
+
+}
